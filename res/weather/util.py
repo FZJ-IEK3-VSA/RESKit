b3ffa53dd4d0d08da8722b1f82a32c1ee166582f
Severin <s.ryberg@fz-juelich.de>
2017-08-11 03:04:16 +0200

    Major restcturein light of Climate Data Operators, needs testing

--- res/weather/util.py
+++ res/weather/util.py
@@ -2,48 +2,231 @@ import numpy as np
 import netCDF4 as nc
 import geokit as gk
 import ogr, osr
+from os import listdir
 from os.path import join, isfile, dirname, basename
 from glob import glob
 import pandas as pd
 from collections import namedtuple, OrderedDict
 from scipy.interpolate import RectBivariateSpline, interp2d
+import types
 
-Index = namedtuple("Index", "yi xi")
-Location = namedtuple("Location", "x y")
 
 # making an error
-class ResWeatherError(Exception): pass # this just creates an error that we can use
-
-# Make a single data container
-class GenericElement(object):
-    def __init__(s, name, data, times, xCoords, yCoords, coordSRS, meta=None):
-        s.data = data
-        s.times = times
-        s.xCoords = xCoords
-        s.yCoords = yCoords
-
-        s._xMin = s.xCoords.min()
-        s._xMax = s.xCoords.max()
-        s._yMin = s.yCoords.min()
-        s._yMax = s.yCoords.max()
-
-        s.coordSRS = coordSRS
-        s.name = name
-
-        s.meta = OrderedDict()
-        if not meta is None:
-            s.meta.update(meta)
-
-        s._interpolation = 'near'
-
-    def setInterpolationMode(s, mode=None):
-        if mode is None: mode='near'
-        if not mode in ['near', "cubic", "linear"]:
-            raise ResWeatherError("mode not recognized")
-
-        s._interpolation = mode
+class ResError(Exception): pass # this just creates an error that we can use
 
 
+# Make some type-helpers
+Index = namedtuple("Index", "yi xi")
+Location = namedtuple("Location", "x y")
+Bounds = namedtuple("Bounds","lonMin latMin lonMax latMax")
+
+def _ensureList(locations):
+    # Ensure loc is a list
+    if isinstance(locations, types.ListType):
+        pass    
+    elif isinstance(locations, types.GeneratorType):
+        locations = list(locations)
+    else:
+        locations = [locations, ]
+    # Done!
+    return locations
+
+def _ensureGeom(locations):
+    if isinstance(locations,types.ListType):
+        if isinstance(locations[0], ogr.Geometry): # Check if loc is a list of point
+            if not locations[0].GetSpatialReference().IsSame(gk.srs.EPSG4326):
+                locations = gk.geom.transform(locations, toSRS=gk.srs.EPSG4326)
+        elif isinstance(locations[0], Location):
+            locations = [gk.geom.point(loc.x, loc.y, srs=gk.srs.EPSG4326) for loc in locations]
+        else:
+            raise ResError("Cannot understand location input. Use either a Location or an ogr.Geometry object")
+
+    elif isinstance(locations, types.GeneratorType):
+        locations = _ensureGeom(list(locations))
+
+    elif isinstance(locations, ogr.Geometry): # Check if loc is a single point
+        if not locations.GetSpatialReference().IsSame(gk.srs.EPSG4326):
+            locations = locations.Clone()
+            locations.TransformTo(gk.srs.EPSG4326)
+
+    elif isinstance(locations, Location):
+        locations = gk.geom.point(locations.x, locations.y, srs=gk.srs.EPSG4326)
+    else:
+        raise ResError("Cannot understand location input. Use either a Location or an ogr.Geometry object")
+
+    # Done!
+    return locations
+
+def _ensureLoc(locations):
+    if isinstance(locations,types.ListType):
+        if isinstance(locations[0], ogr.Geometry): # Check if loc is a list of point
+            if not locations[0].GetSpatialReference().IsSame(gk.srs.EPSG4326):
+                locations = gk.geom.transform(locations, toSRS=gk.srs.EPSG4326)
+            locations = [Location(x=l.GetX(), y=l.GetY()) for l in locations]
+        elif isinstance(locations[0], Location):
+            pass
+        else:
+            raise ResError("Cannot understand location input. Use either a Location or an ogr.Geometry object")
+
+    elif isinstance(locations, types.GeneratorType):
+        locations = _ensureLoc(list(locations))
+
+    elif isinstance(locations, ogr.Geometry): # Check if loc is a single point
+        if not locations.GetSpatialReference().IsSame(gk.srs.EPSG4326):
+            locations = locations.Clone()
+            locations.TransformTo(gk.srs.EPSG4326)
+            locations = Location(x=locations.GetX(), y=locations.GetY())
+
+    elif isinstance(locations, Location):
+        pass
+    else:
+        raise ResError("Cannot understand location input. Use either a Location or an ogr.Geometry object")
+
+    # Done!
+    return locations
+
+# context mean
+def computeContextMean(source, contextArea, fillnan=True, pixelSize=None, srs=None):
+    if pixelSize is None and srs is None:
+        ras = gk.raster.describe(source)
+        pixelSize=(ras.dx, ras.dy)
+        srs=ras.srs
+
+    # get all GWA values in the area
+    rm = gk.RegionMask.fromGeom(contextArea, pixelSize=pixelSize, srs=srs)
+    values = rm.warp(source)
+    values[values<0] = np.nan # set the no data value so they can be filled properly
+
+    # fill all no data with the closest values
+    if fillnan:
+        while np.isnan(values[rm.mask]).any():
+            tmp = values.copy()
+            for yi,xi in np.argwhere(np.isnan(values)):
+                vals = np.array([np.nan, np.nan, np.nan, np.nan])
+                if yi>0:    vals[0] = values[yi-1,xi]
+                if xi>0:    vals[1] = values[yi,xi-1]
+                if yi<yN-1: vals[2] = values[yi+1,xi]
+                if xi<xN-1: vals[3] = values[yi,xi+1]
+
+                if (~np.isnan(vals)).any():
+                    tmp[yi,xi] = np.nanmean(vals)
+            values = tmp
+    # done!
+    return np.nanmean(gvalues[rm.mask])
+
+
+# make a data handler
+class NCSource(object):
+    def __init__(s, path, bounds=None, timeName="time", latName="lat", lonName="lon"):
+        # set basic vairables 
+        s.path = path
+        s.ds = nc.dataset
+        s._allLats = s.ds[latName][:]
+        s._allLons = s.ds[lonName][:]
+
+        # set lat and lon selections
+        if not bounds is None:
+            if not isinstance(bounds, bounds):
+                bounds = Bounds(*bounds)
+                print("bounds input is not a 'Bounds' type. Using it is safer!")
+            
+            # find slices
+            s._lonSel = s.allLons >= bounds.lonMin and s.allLons <= bounds.lonMax
+            s._latSel = s.allLats >= bounds.latMin and s.allLats <= bounds.latMax
+        else:
+            s._latSel = np.s_[:]
+            s._lonSel = np.s_[:]
+
+        s.lats = s._allLats[s._latSel]
+        s.lons = s._allLons[s._lonSel]
+
+        # compute time index
+        s.timeindex = nc.num2date(s.ds[timeName][:], s.ds[timeName].units)
+
+        # initialize some variables
+        s._transformCount
+        s.data = OrderedDict()
+
+    def load(s, variable, name=None, processor=None):
+        # read the data
+        if not variable in s.ds.variables.keys():
+            raise ResError(variable+" not is source")
+        tmp = s.ds[variable][:,s._latSel,s._lonSel]
+
+        # process, maybe?
+        if not processor is None:
+            tmp = processor(tmp)
+
+        # save the data
+        if name is None: name = variable
+        s.data[variable] = tmp
+
+    def get(s, variable, locations, interpolation='near'):
+        # Ensure loc is a list
+        locations = _ensureLoc(_ensureList(locations))
+
+        # Do interpolation
+        lonCoords = np.array([loc.x for loc in locations])
+        latCoords = np.array([loc.y for loc in locations])
+        if interpolation == 'near':
+            # compute the closest indecies
+            lonIndecies = [np.argmin(np.abs(lon-s.lons)) for lon in lonCoords]
+            latIndecies = [np.argmin(np.abs(lat-s.lats)) for lat in latCoords]
+
+            # arrange the output data
+            output = [s.Data[:, latIndex, lonIndex] for latIndex, lonIndex in zip (latIndecies, lonIndecies)]
+
+        else:
+            raise ResError("No other interpolation schemes are implemented at this time :(")
+
+        # Make output as Series objects
+        if len(output) == 1:
+            return pd.Series(output[0], index=s.times, name=variable)
+        else: 
+            return pd.DataFrame(np.column_stack(output), index=s.times, columns=locations)
+
+    def contextArea(s,location):
+        # Ensure we have a Location
+        if isinstance(location, ogr.Geometry):
+            # test if location is in lat & lon coordinates
+            if not location.GetSpatialReference().IsSame(gk.srs.EPSG4326):
+                location.TransformTo(gk.srs.EPSG4326)
+            location = [Location(x=loc.GetX(), y=loc.GetY()) for loc in locations]
+        elif not isinstance(locations, Location):
+            raise ResError("Cannot understand location input. Use either a Location or an ogr.Geometry object")
+
+        # Get closest indexes
+        lonI = np.argmin(np.abs(location.x-s.lons))
+        latI = np.argmin(np.abs(location.y-s.lats))
+
+        # get area
+        return s._contextAreaAt(latI, lonI)
+
+    def _contextAreaAt(s, latI, lonI):
+
+        # Make and return a box
+        lowLat = (s.lats[latI]+s.lats[latI-1])/2
+        highLat = (s.lats[latI]+s.lats[latI+1])/2
+        lowLon = (s.lons[lonI]+s.lons[lonI-1])/2
+        highLon = (s.lons[lonI]+s.lons[lonI+1])/2
+        
+        return gk.box( lowLon, lowLat, highLon, highLat, srs=gk.srs.EPSG4326 )
+
+    def computeContextMeans(s, source, output=None, fillnan=True):
+        # get rastr info
+        ras = gk.raster.describe(source)
+
+        # compute all means
+        means = np.zeros((s.lats.size, s.lons.size))
+        for latI,lat in enumerate(s.lats):
+            for lonI,lon in enumerate(s.lons):        
+                means[latI,lonI] = computeContextMean(source=source, contextArea=s._contextAreaAt(latI,lonI), 
+                                                      pixelSize=(ras.dx, ras.dy), srs=ras.srs, 
+                                                      fillnan=fillnan)
+
+        return means
+
+'''
     def __getitem__(s,loc):
         # Check if the location input is a point geometry
         if isinstance(loc, ogr.Geometry):
@@ -110,99 +293,4 @@ class GenericElement(object):
             outdata = [interpolate(i) for i in range(s.times.shape[0])]
 
         return pd.Series(outdata, index=s.times, name=s.name)
-        
-# make a generic datasource
-class GenericSource(object):
-    _GWA50_AVERAGE_SOURCE = None
-    _GWA100_AVERAGE_SOURCE = None
-    _WINDSPEED_NORMALIZER_SOURCE = None
-    _WINDSPEED_NORMALIZER_VAR = None
-
-    def __init__(s, timeframe, xCoords, yCoords, coordSRS='latlon'):
-        s.source = "unknown"
-
-        # arrange time index
-        if isinstance(timeframe,int):
-            s.timeStart = pd.Timestamp(year=timeframe, month=1, day=1, hour=0)
-            s.timeEnd = pd.Timestamp(year=timeframe, month=12, day=31, hour=23)
-        else:
-            s.timeStart = pd.Timestamp(timeframe[0])
-            s.timeEnd = pd.Timestamp(timeframe[1])
-
-        # arange point coordinates
-        s.coordSRS = gk.srs.loadSRS(coordSRS)
-
-        s.xCoords = np.array(xCoords)
-        s.yCoords = np.array(yCoords)
-
-        if len(xCoords.shape) == 1: # make it two dimensional
-            s.xCoords, s.yCoords = np.meshgrid(s.xCoords, s.yCoords)
-
-        s._xMin = s.xCoords.min()
-        s._xMax = s.xCoords.max()
-        s._yMin = s.yCoords.min()
-        s._yMax = s.yCoords.max()
-
-        # initialize an empty data container
-        s.data = OrderedDict()
-        s.dataShape = xCoords.shape
-
-    def loc2Index(s, loc):
-        # Check if the location input is a point geometry
-        if isinstance(loc, ogr.Geometry):
-            if not loc.GetGeometryName()=="POINT":
-                raise ResWeatherError("location geometry is not a Point object")
-            
-            # make sure the input is in the correct reference system
-            locSRS = loc.GetSpatialReference()
-            if not locSRS is None: 
-                if not locSRS.IsSame(s.coordSRS):
-                    loc.TransformTo(s.coordSRS)
-
-            # Get x and y coordinates
-            x = loc.GetX()
-            y = loc.GetY()
-
-        else: # input should be a tuple in the correct SRS
-            x = loc[0]
-            y = loc[1]
-            
-        # make sure we're in range
-        if not (x>=s._xMin and x<=s._xMax): raise ResWeatherError("Input exceeds X boundary")
-        if not (y>=s._yMin and y<=s._yMax): raise ResWeatherError("Input exceeds Y boundary")
-
-        # Compute the best indicies
-        xDist = x - s.xCoords
-        yDist = y - s.yCoords
-
-        totalDist2 = xDist*xDist + yDist*yDist
-
-        yi,xi = np.argwhere(totalDist2 == np.min(totalDist2))[0]
-
-        # done!
-        return Index(yi,xi)
-
-    def gwaContextAverage(s, loc, height=50):
-        if height == 50:
-            return gk.raster.extractValues(s._GWA50_AVERAGE_SOURCE, loc).data
-        elif height == 100:
-            return gk.raster.extractValues(s._GWA100_AVERAGE_SOURCE, loc).data
-        else:
-            raise ResWeatherError("height is not 50 or 100")
-
-    def setInterpolationMode(s, mode=None):
-        for k,v in s.data.items():
-            v.setInterpolationMode(mode)
-
-    def setElement(s, name, data, times, meta=None):
-        s.data[name] = GenericElement(name, data, times, xCoords=s.xCoords, yCoords=s.yCoords, coordSRS=s.coordSRS, meta=meta)
-
-    def __getitem__(s, i):
-        if isinstance(i, str):
-            return s.data[i]
-        else:
-            output = OrderedDict()
-            for k,v in s.data.items():
-                output[k] = v[i]
-
-            return pd.DataFrame(output)
+'''
--- res/weather/windutil.py
+++ res/weather/windutil.py
@@ -1,101 +1,64 @@
 from .util import *
 
-def fixLoc(loc):
+################################################################################
+## Spatial adjustment methods
+
+def adjustLraToGwa( windspeed, targetLoc, gwa, longRunAverage):
     ## Ensure location is okay
-    if isinstance(loc, ogr.Geometry): # Check if loc is a single point
-        if not loc.GetSpatialReference().IsSame(gk.srs.EPSG4326):
-            loc = loc.TransformTo(gk.srs.EPSG4326) # make sure the point is in lat-lon
-    elif isinstance(loc, tuple) and len(loc)==2: # Check if loc is a single tuple
-        lon,lat = loc
-        loc = gk.geom.makePoint(float(lon), float(lat), srs=gk.srs.EPSG4326)
-    else: # assume loc is iterable and so lets make it into a series of points
-        loc = [gk.geom.makePoint(float(lon), float(lat), srs=gk.srs.EPSG4326) for lon,lat in loc]
+    targetLoc = _ensureList(_ensureGeom(targetLoc))
 
-    return loc
-
-def spatialAdjustmentByGWA(ws, targetLoc, gwa, gwaMean=None, longRunAverage=None, contextArea=None):
-    # check inputs
-    if (gwaMean is None and longRunAverage is None and contextArea is None) or \
-       (not gwaMean is None and not longRunAverage is None and not contextArea is None) :
-        raise RuntimeError("Exactly one of gwaMean, longRunAverage or contextArea must be defined")
-
-    ## Ensure location is okay
-    targetLoc = fixLoc(targetLoc)
-    
     # Get the local gwa value
-    gwaLocValue = gk.raster.extractValues(gwa, targetLoc).data
+    gwaLocValue = np.array([x.data for x in gk.raster.extractValues(gwa, targetLoc)])
+    
+    # Get the long run average value
+    if isinstance(longRunAverage, str): # A path to a raster dataset has been given
+        longRunAverage = np.array([x.data for x in gk.raster.extractValues(longRunAverage, targetLoc)])
 
-    # Apply methods
-    if not contextArea is None: # The user has chosen the GWA relative method, but the GWA average in the context area
-                                #  needs to be determined
-        if isinstance(contextArea, tuple): # context area is a set lat/lon boundaries, make them into a geometry 
-            contextArea = gk.geom.makeBox(contextArea, srs=gk.srs.EPSG4326)
+    # apply adjustment
+    if isinstance(windspeed, NCSource):
+        windspeed = NCSource.get("windspeed", targetloc)
 
-        if not isinstance(contextArea, ogr.Geometry) or contextArea.GetSpatialReference() is None:
-            raise RuntimeError("contextArea must be an OGR Geometry object with an SRS or a set of lat/lon boundaries")  
-        
-        # get all GWA values in the area
-        rm = gk.RegionMask.fromGeom(contextArea, pixelSize=0.01, srs=gk.srs.EPSG4326)
-        gwaValues = rm.warp(gwa)
-        gwaValues[gwaValues==-999] = np.nan # set the no data value so they can be filled properly
+    return windspeed * (gwaLocValue / longRunAverage)
 
-        # fill all no data with the closest values
-        while np.isnan(gwaValues[rm.mask]).any():
-            tmp = gwaValues.copy()
-            for yi,xi in np.argwhere(np.isnan(gwaValues)):
-                vals = np.array([np.nan, np.nan, np.nan, np.nan])
-                if yi>0:    vals[0] = gwaValues[yi-1,xi]
-                if xi>0:    vals[1] = gwaValues[yi,xi-1]
-                if yi<yN-1: vals[2] = gwaValues[yi+1,xi]
-                if xi<xN-1: vals[3] = gwaValues[yi,xi+1]
+def adjustContextMeanToGwa( windspeed, targetLoc, gwa, contextMean=None):
+    ## Ensure location is okay
+    targetLoc = _ensureList(_ensureGeom(targetLoc))
 
-                if (~np.isnan(vals)).any():
-                    tmp[yi,xi] = np.nanmean(vals)
-            gwaValues = tmp
+    # Get the local gwa value
+    gwaLocValue = np.array([x.data for x in gk.raster.extractValues(gwa, targetLoc)])
 
-        # get mean
-        gwaMean = gwaValues[rm.mask].mean()
+    # Get the gwa contextual mean value
+    if contextMean is None: # the contexts needs to be computed
+        # this only works when windspeed is an NCSource object
+        contextMean = np.array([windspeed.contextAreaAt(loc) for loc in targetLoc])
 
-    if not gwaMean is None : # the user has chosen the GWA-relative method
-        if isinstance(gwaMean, str): # A path to a raster dataset has been given
-            gwaMean = gk.raster.extractValues(gwaMean, targetLoc).data
-        return ws * (gwaLocValue / gwaMean)
+    elif isinstance(context, str): # A path to a raster dataset has been given to read the means from
+        contextMean = np.array([x.data for x in gk.raster.extractValues(context, targetLoc)])
 
-    else: # the user has chosen the GWA-normalized method
-        if isinstance(longRunAverage, str): # A path to a raster dataset has been given
-            longRunAverage = gk.raster.extractValues(longRunAverage, targetLoc).data
-        return ws * (gwaLocValue / longRunAverage)
+    # apply adjustment    
+    if isinstance(windspeed, NCSource):
+        windspeed = NCSource.get("windspeed", targetloc)
 
+    return windspeed * (gwaLocValue / context)
 
-def projectByLogLaw(windspeed, measuredHeight, targetHeight, roughness):
+################################################################################
+## Vertical projection methods
+
+def projectByLogLaw( windspeed, measuredHeight, targetHeight, roughness):
     return windspeed * np.log(targetHeight/roughness) / np.log(measuredHeight/roughness)
 
-def projectByPowerLaw(windspeed, measuredHeight, targetHeight, alpha):
+def projectByPowerLaw( windspeed, measuredHeight, targetHeight, alpha):
     return windspeed * np.power(targetHeight/measuredHeight, alpha)
-    
-def projectByGWA(windspeed, targetHeight, measuredHeight, gwaDir, loc, method="log", value=0):
-    # determine method and project
-    if method == 'log':
-        roughness = computeRoughnessFromGWA(gwaDir, loc)[value]
-        wsTarget = projectByLogLaw(windspeed, measuredHeight, targetHeight, roughness)
-    elif method == 'power':
-        alpha = computeAlphaFromGWA(gwaDir, loc)[value]
-        wsTarget = projectByPowerLaw(windspeed, measuredHeight, targetHeight, alpha)
 
-    # done!
-    return wsTarget
+################################################################################
+## Alpha computers
 
-def projectByCLC(windspeed, targetHeight, measuredHeight, clcPath, loc):
-    roughness = computeRoughnessFromCLC(loc, clcPath)
-    wsTarget = projectByLogLaw(windspeed, measuredHeight, targetHeight, roughness)
-    
-    # done!
-    return wsTarget
+def alphaFromLevels( lowWindSpeed, lowHeight, highWindSpeed, highHeight):
+    return np.log(lowWindSpeed/highWindSpeed)/np.log(lowHeight/highHeight)
 
-
-def computeRoughnessFromGWA(gwaDir, loc):
+def alphaFromGWA( gwaDir, loc, pairID=2):
     ## Ensure location is okay
-    loc = fixLoc(loc)
+    loc = _ensureList(_ensureGeom(loc))
 
     # Get the GWA averages
     GWA_files = [join(gwaDir, "WS_050m_global_wgs84_mean_trimmed.tif"),
@@ -107,9 +70,9 @@ def computeRoughnessFromGWA(gwaDir, loc):
             raise ResWeatherError("Could not find file: "+f)
 
     try:
-        gwaAverage50 =  gk.raster.extractValues(GWA_files[0], loc, noDataOkay=False).data
-        gwaAverage100 = gk.raster.extractValues(GWA_files[1], loc, noDataOkay=False).data
-        gwaAverage200 = gk.raster.extractValues(GWA_files[2], loc, noDataOkay=False).data
+        if pairID==0 or pairID==1: gwaAverage50  = np.array([x.data for x in gk.raster.extractValues(GWA_files[0], loc, noDataOkay=False)])
+        if pairID==1 or pairID==2: gwaAverage100 = np.array([x.data for x in gk.raster.extractValues(GWA_files[1], loc, noDataOkay=False)])
+        if pairID==0 or pairID==2: gwaAverage200 = np.array([x.data for x in gk.raster.extractValues(GWA_files[2], loc, noDataOkay=False)])
 
     except gk.util.GeoKitRasterError as e:
         if str(e) == "No data values found in extractValues with 'noDataOkay' set to False":
@@ -118,45 +81,53 @@ def computeRoughnessFromGWA(gwaDir, loc):
             raise e
 
     # Interpolate gwa average to desired height
-    r_50_100 = np.exp( (gwaAverage100 * np.log(50) - gwaAverage50 * np.log(100) )/(gwaAverage100 - gwaAverage50) )
-    r_100_200 = np.exp( (gwaAverage200 * np.log(100) - gwaAverage100 * np.log(200) )/(gwaAverage200 - gwaAverage100) )
-    r_50_200 = np.exp( (gwaAverage200 * np.log(50) - gwaAverage50 * np.log(200) )/(gwaAverage200 - gwaAverage50) )
-
-    # done!
-    return (r_50_100, r_100_200, r_50_200)
-
-def computeAlphaFromGWA(gwaDir, loc):
-    ## Ensure location is okay
-    loc = fixLoc(loc)
-
-    # Get the GWA averages
-    GWA_files = [join(gwaDir, "WS_050m_global_wgs84_mean_trimmed.tif"),
-                 join(gwaDir, "WS_100m_global_wgs84_mean_trimmed.tif"),
-                 join(gwaDir, "WS_200m_global_wgs84_mean_trimmed.tif")]
-
-    for f in GWA_files: 
-        if not isfile(f): 
-            raise ResWeatherError("Could not find file: "+f)
-
-    try:
-        gwaAverage50 =  gk.raster.extractValues(GWA_files[0], loc, noDataOkay=False).data
-        gwaAverage100 = gk.raster.extractValues(GWA_files[1], loc, noDataOkay=False).data
-        gwaAverage200 = gk.raster.extractValues(GWA_files[2], loc, noDataOkay=False).data
-
-    except gk.util.GeoKitRasterError as e:
-        if str(e) == "No data values found in extractValues with 'noDataOkay' set to False":
-            raise ResWeatherError("The given point does not appear to have valid data in the Global Wind Atlas dataset")
-        else:
-            raise e
-
-    # Interpolate gwa average to desired height
-    a_50_100 = np.log(gwaAverage50/gwaAverage100)/np.log(50.0/100)
-    a_100_200 = np.log(gwaAverage100/gwaAverage200)/np.log(100.0/200)
-    a_50_200 = np.log(gwaAverage50/gwaAverage200)/np.log(50.0/200)
+    if pairID==0 or pairID==1: out = alphaFromLevels(gwaAverage50,50,gwaAverage100,100)
+    if pairID==1 or pairID==2: out = alphaFromLevels(gwaAverage100,100,gwaAverage200,200)
+    if pairID==0 or pairID==2: out = alphaFromLevels(gwaAverage50,50,gwaAverage200,200)
        
     # done!
-    return a_50_100, a_100_200, a_50_200
+    if out.size==1: return out[0]
+    else: return out
 
+################################################################################
+## Roughness computers
+
+def roughnessFromLevels(lowWindSpeed, lowHeight, highWindSpeed, highHeight):
+    return np.exp( (highWindSpeed * np.log(lowHeight) - lowWindSpeed * np.log(highHeight) )/(highWindSpeed - lowWindSpeed) )
+
+def roughnessFromGWA(gwaDir, loc, pairID=2):
+    ## Ensure location is okay
+    loc = _ensureList(_ensureGeom(loc))
+
+    # Get the GWA averages
+    GWA_files = [join(gwaDir, "WS_050m_global_wgs84_mean_trimmed.tif"),
+                 join(gwaDir, "WS_100m_global_wgs84_mean_trimmed.tif"),
+                 join(gwaDir, "WS_200m_global_wgs84_mean_trimmed.tif")]
+
+    for f in GWA_files: 
+        if not isfile(f): 
+            raise ResWeatherError("Could not find file: "+f)
+
+    try:
+        if pairID==0 or pairID==1: gwaAverage50  = np.array([x.data for x in gk.raster.extractValues(GWA_files[0], loc, noDataOkay=False)])
+        if pairID==1 or pairID==2: gwaAverage100 = np.array([x.data for x in gk.raster.extractValues(GWA_files[1], loc, noDataOkay=False)])
+        if pairID==0 or pairID==2: gwaAverage200 = np.array([x.data for x in gk.raster.extractValues(GWA_files[2], loc, noDataOkay=False)])
+
+
+    except gk.util.GeoKitRasterError as e:
+        if str(e) == "No data values found in extractValues with 'noDataOkay' set to False":
+            raise ResWeatherError("The given point does not appear to have valid data in the Global Wind Atlas dataset")
+        else:
+            raise e
+
+    # Interpolate gwa average to desired height
+    if pairID==0 or pairID==1: out = roughnessFromLevels(gwaAverage50,50,gwaAverage100,100)
+    if pairID==1 or pairID==2: out = roughnessFromLevels(gwaAverage100,100,gwaAverage200,200)
+    if pairID==0 or pairID==2: out = roughnessFromLevels(gwaAverage50,50,gwaAverage200,200)
+
+    # done!
+    if out.size==1: return out[0]
+    else: return out
 
 ############################################################################
 ## See CLC codes at: http://uls.eionet.europa.eu/CLC2000/classes/
@@ -255,12 +226,16 @@ _clcGridToCode_v2006[42] = 521
 _clcGridToCode_v2006[43] = 522
 _clcGridToCode_v2006[44] = 523
 
-def computeRoughnessFromCLC(s, loc, clcPath):
+def roughnessFromCLC(loc, clcPath):
     ## Ensure location is okay
-    loc = fixLoc(loc)
+    loc = _ensureList(_ensureGeom(loc))
 
     ## Get pixels values from clc
-    clcGridValue = gk.raster.extractValues(clcPath, loc, noDataOkay=False).data
+    clcGridValues = np.array([x.data for x in gk.raster.extractValues(clcPath, loc, noDataOkay=False)])
 
     ## Get the associated
-    return clcCodeToRoughess[_clcGridToCode_v2006[clcGridValue]]
+    outputs = [clcCodeToRoughess[_clcGridToCode_v2006[ val ]] for val in clcGridValue]
+
+    ## Done!
+    if len(outputs)==1: return outputs[0]
+    else: return np.array(outputs)
