8c7c62c08ccc09019ffa4ffaf84d1f32b6925ca9
Severin Ryberg <sevberg129@gmail.com>
2018-12-13 08:31:21 +0100

    added some tests

---
+++ res/test/test_util.py
@@ -0,0 +1,18 @@
+def test_storeTimeseriesAsNc():
+    print("test_storeTimeseriesAsNc not tested...")
+
+def test_removeLeapDay():
+    print("test_removeLeapDay not tested...")
+
+def test_linearTransition():
+    print("test_linearTransition not tested...")
+
+def test_parseRESGenerationFile():
+    print("test_parseRESGenerationFile not tested...")
+
+
+if __name__=="__main__":
+    test_storeTimeseriesAsNc()
+    test_removeLeapDay()
+    test_linearTransition()
+    test_parseRESGenerationFile()
