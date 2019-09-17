8de4175e73859349d2396a2da75bc26c4e138a7c
Severin <s.ryberg@fz-juelich.de>
2019-09-17 23:15:58 +0200

    worked on examples
    Some are not working still, and more need to be added

--- res/__init__.py
+++ res/__init__.py
@@ -2,4 +2,12 @@ from res import util
 from res import weather
 from res import economic
 
+# Add useful paths for testing and stuff
+from collections import OrderedDict as _OrderedDict
+from glob import glob as _glob
+from os.path import join as _join, dirname as _dirname, basename as _basename
 
+_TEST_DATA_ = _OrderedDict()
+
+for f in _glob(_join(_dirname(__file__), "test", "data", "*")):
+    _TEST_DATA_[_basename(f)] = f
