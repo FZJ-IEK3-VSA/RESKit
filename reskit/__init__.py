c57b2902462752513678322ed8403d05557e712c
Severin <s.ryberg@fz-juelich.de>
2020-04-23 23:05:50 +0200

    restructured util core

--- reskit/__init__.py
+++ reskit/__init__.py
@@ -1,15 +1,17 @@
 __version__ = "0.1.2"
 
-from reskit import util
-from reskit import weather
-from reskit import economic
+from . import core
 
-# Add useful paths for testing and stuff
-from collections import OrderedDict as _OrderedDict
-from glob import glob as _glob
-from os.path import join as _join, dirname as _dirname, basename as _basename
+# from reskit import util
+# from reskit import weather
+# from reskit import economic
 
-_TEST_DATA_ = _OrderedDict()
+# # Add useful paths for testing and stuff
+# from collections import OrderedDict as _OrderedDict
+# from glob import glob as _glob
+# from os.path import join as _join, dirname as _dirname, basename as _basename
 
-for f in _glob(_join(_dirname(__file__), "test", "data", "*")):
-    _TEST_DATA_[_basename(f)] = f
+# _TEST_DATA_ = _OrderedDict()
+
+# for f in _glob(_join(_dirname(__file__), "test", "data", "*")):
+#     _TEST_DATA_[_basename(f)] = f
