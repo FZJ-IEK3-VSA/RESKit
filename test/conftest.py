0872b48a9fb004eadae99ebc4ba21b2f46c75166
Severin <s.ryberg@fz-juelich.de>
2020-04-24 09:41:31 +0200

    util pytest working

--- test/conftest.py
+++ test/conftest.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 import reskit as rk
 import pytest
-from reskit.workflows.wind import WindWorkflowGenerator
+# from reskit.workflows.wind import WindWorkflowGenerator
 
 
-@pytest.fixture
-def placements():
-    placements = pd.read_csv(rk._TEST_DATA_['turbine_placements.csv'])
-    return placements
+# @pytest.fixture
+# def placements():
+#     placements = pd.read_csv(rk._TEST_DATA_['turbine_placements.csv'])
+#     return placements
 
 
-@pytest.fixture
-def wind_workflow(placements):
-    wind_workflow = WindWorkflowGenerator(placements)
-    return wind_workflow
+# @pytest.fixture
+# def wind_workflow(placements):
+#     wind_workflow = WindWorkflowGenerator(placements)
+#     return wind_workflow
