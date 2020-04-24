178da0a18877991718d81d41a6c4f119bc2b2fd6
Severin <s.ryberg@fz-juelich.de>
2020-04-24 09:28:58 +0200

    added util testing

--- reskit/__init__.py
+++ reskit/__init__.py
@@ -2,16 +2,4 @@ __version__ = "0.1.2"
 
 from . import core
 
-# from reskit import util
-# from reskit import weather
-# from reskit import economic
-
-# # Add useful paths for testing and stuff
-# from collections import OrderedDict as _OrderedDict
-# from glob import glob as _glob
-# from os.path import join as _join, dirname as _dirname, basename as _basename
-
-# _TEST_DATA_ = _OrderedDict()
-
-# for f in _glob(_join(_dirname(__file__), "test", "data", "*")):
-#     _TEST_DATA_[_basename(f)] = f
+from .test import TEST_DATA
---
+++ test/core/util/test_air_density.py
@@ -0,0 +1,7 @@
+import numpy as np
+from reskit.core.util.air_density import compute_air_density
+
+
+def test_compute_air_density():
+    assert np.isclose(compute_air_density(), 1.2050461878674021)
+    assert np.isclose(compute_air_density(temperature=30, pressure=105000, relative_humidity=0.3), 1.2021773918072405)
