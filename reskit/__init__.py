8323317afd97effb130872bd20066051c6df4eef
Severin <s.ryberg@fz-juelich.de>
2020-06-17 10:22:13 +0200

    Added simulation queuer

--- reskit/__init__.py
+++ reskit/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.2.0"
 
 from . import util
 from . import weather
-# from .workflow_manager import WorkflowManager
+from .workflow_manager import WorkflowManager, WorkflowQueue
 from . import wind
 from . import solar
 
