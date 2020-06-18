5eddc5f45059073fc0b819c4874004f2ef1b65ed
nlandwehrfzj <62711124+nlandwehrfzj@users.noreply.github.com>
2020-06-18 10:56:04 +0200

    Restruct (#24)
    Solar Docstrings
    
    Co-authored-by: Natascha Landwehr <n.landwehr@iwv3.kfa-juelich.de>
    Co-authored-by: Severin <s.ryberg@fz-juelich.de>

--- reskit/solar/core/system_design.py
+++ reskit/solar/core/system_design.py
@@ -7,34 +7,34 @@ from ...util import ResError
 
 def location_to_tilt(locs, convention="Ryberg2020", **kwargs):
     """
+    def location_to_tilt(locs, convention="Ryberg2020", **kwargs)
+    
     Simple system tilt estimator based off latitude and longitude coordinates
 
 
     Parameters
     ----------
     locs : geokit.LocationSet or iterable of (lat,lon) pairs
-        The locations at which to estimate system tilt angle
+           The locations at which to estimate system tilt angle
 
     convention : str, optional
-        The calculation method used to suggest system tilts
-
-        `convention` can be...
-            * "Ryberg2020"
-            * A string consumable by 'eval'
-                - Can use the variable 'latitude'
-                - Ex. "latitude*0.76"
-            * A path to a raster file
-
-    **kwargs
-        Optional keyword arguments to use in geokit.raster.interpolateValues(...)
-        * Only applies when `convention` is a path to a raster file
+                 The calculation method used to suggest system tilts
+                 Options are:
+                     * "Ryberg2020"
+                     * A string consumable by 'eval'
+                     - Can use the variable 'latitude'
+                     - Ex. "latitude*0.76"
+                     * A path to a raster file
+                     
+    kwargs: Optional keyword arguments to use in geokit.raster.interpolateValues(...).
+            Only applies when `convention` is a path to a raster file
 
 
     Returns
     -------
     np.ndarray
-        Suggested tilt angle at each of the provided `locs`
-        - Has the same length as the number of `locs`
+        Suggested tilt angle at each of the provided `locs`.
+        Has the same length as the number of `locs`.
 
     Notes
     -----
