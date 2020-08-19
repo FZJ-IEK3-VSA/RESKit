389832a50778bc048ff4118d4eb208f2f007540f
Severin <s.ryberg@fz-juelich.de>
2020-08-19 13:22:29 +0200

    WorkflowManager tests

--- reskit/__init__.py
+++ reskit/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.2.0"
 
 from . import util
 from . import weather
-from .workflow_manager import WorkflowManager, WorkflowQueue
+from .workflow_manager import WorkflowManager, WorkflowQueue, distribute_workflow
 from . import wind
 from . import solar
 
