b070d31ae27327022073be71dedbd8cfbf5af641
Severin Ryberg <s.ryberg@fz-juelich.de>
2020-09-15 17:41:47 +0200

    re-added vaidator function

--- reskit/wind/workflows/workflows.py
+++ reskit/wind/workflows/workflows.py
@@ -252,3 +252,149 @@ def onshore_wind_era5_unvalidated(placements, era5_path, gwa_100m_path, esa_cci_
     return wf.to_xarray(output_netcdf_path=output_netcdf_path, output_variables=output_variables)
 
 
+def onshore_wind_era5_validator(placements, era5_path, gwa_100m_path, esa_cci_path, convolution_scaling_factors=[0.06], convolution_base_factors=[0.1], loss_sharpness_factors=[5.0], loss_base_factors=[0.0], wind_speed_offsets=[0], wind_speed_scalings=[1.0]):
+    """
+    Simulates onshore wind generation using ECMWF's ERA5 database [1]
+
+    Parameters
+    ----------
+    placements : pandas Dataframe
+        A Dataframe object with the parameters needed by the simulation.
+    era5_path : str
+        Path to the ERA5 data.
+    gwa_100m_path : str
+        Path to the Global Wind Atlas at 100m [2] rater file.
+    esa_cci_path : str
+        Path to the ESA CCI raster file [3].
+    output_netcdf_path : str, optional
+        Path to a directory to put the output files, by default None
+    output_variables : str, optional
+        Restrict the output variables to these variables, by default None
+
+    Returns
+    -------
+    xarray.Dataset
+        A xarray dataset including all the output variables you defined as your output variables.
+
+    Sources
+    ------
+    [1] European Centre for Medium-Range Weather Forecasts. (2019). ERA5 dataset. https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5
+    [2] DTU Wind Energy. (2019). Global Wind Atlas. https://globalwindatlas.info/
+    [3] ESA. Land Cover CCI Product User Guide Version 2. Tech. Rep. (2017). Available at: maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-Ph2-PUGv2_2.0.pdf
+    """
+    from collections import OrderedDict
+    from itertools import product
+    from json import dumps
+    import pandas as pd
+    import numpy as np
+
+    wf = WindWorkflowManager(placements)
+
+    wf.read(
+        variables=['elevated_wind_speed',
+                   "surface_pressure",
+                   "surface_air_temperature",
+                   "boundary_layer_height"],
+        source_type="ERA5",
+        source=era5_path,
+        set_time_index=True,
+        verbose=False)
+
+    wf.adjust_variable_to_long_run_average(
+        variable='elevated_wind_speed',
+        source_long_run_average=rk_weather.Era5Source.LONG_RUN_AVERAGE_WINDSPEED,
+        real_long_run_average=gwa_100m_path
+    )
+
+    wf.estimate_roughness_from_land_cover(
+        path=esa_cci_path,
+        source_type="cci")
+
+    wf.logarithmic_projection_of_wind_speeds_to_hub_height(
+            consider_boundary_layer_height=True)
+
+    wf.apply_air_density_correction_to_wind_speeds()
+
+    power_curves = wf.powerCurveLibrary.copy()
+    wind_speeds = wf.sim_data['elevated_wind_speed'].copy()
+
+    outputs = {}
+    for (convolution_scaling_factor,
+        convolution_base_factor) \
+        in product(convolution_scaling_factors,
+                   convolution_base_factors):
+
+        # Reset power curves
+        wf.powerCurveLibrary = power_curves.copy()
+
+        # Apply power curve convolution
+        try:
+            if not (convolution_scaling_factor == 0 and convolution_base_factor == 0):
+                wf.convolute_power_curves(
+                    scaling=convolution_scaling_factor,
+                    base=convolution_base_factor,
+            )
+        except Exception as e:
+            if isinstance(e, KeyboardInterrupt):
+                raise e
+
+            #output = np.full(wf.sim_data['elevated_wind_speed'].shape[0], np.nan)
+            print("  Failed :(")
+            continue
+
+        for (loss_sharpness_factor,
+             loss_base_factor,
+             wind_speed_offset,
+             wind_speed_scaling) \
+            in product(loss_sharpness_factors,
+                       loss_base_factors,
+                       wind_speed_offsets,
+                       wind_speed_scalings):
+
+            print(convolution_scaling_factor,
+                  convolution_base_factor,
+                  loss_sharpness_factor,
+                  loss_base_factor,
+                  wind_speed_offset,
+                  wind_speed_scaling)
+
+            name = dumps({
+                'convolution_scaling_factor': convolution_scaling_factor,
+                'convolution_base_factor': convolution_base_factor,
+                'loss_sharpness_factor': loss_sharpness_factor,
+                'loss_base_factor': loss_base_factor,
+                'wind_speed_offset':wind_speed_offset,
+                'wind_speed_scaling':wind_speed_scaling
+            })
+
+            try:
+
+                # Adjust wind speeds
+                wf.sim_data['elevated_wind_speed'] = np.maximum(
+                        wind_speeds*wind_speed_scaling - wind_speed_offset,
+                        0
+                        )
+
+
+                wf.simulate()
+
+                wf.apply_loss_factor(
+                    loss=lambda x: rk_util.low_generation_loss(
+                        x,
+                        base=loss_base_factor,
+                        sharpness=loss_sharpness_factor)
+                )
+
+                output = wf.sim_data['capacity_factor'].mean(axis=1)
+            except Exception as e:
+                if isinstance(e, KeyboardInterrupt):
+                    raise e
+
+                #output = np.full(wf.sim_data['elevated_wind_speed'].shape[0], np.nan)
+                print("  Failed :(")
+                continue
+
+            outputs[name] = output
+
+    return pd.DataFrame(outputs, index=wf.time_index)
+
