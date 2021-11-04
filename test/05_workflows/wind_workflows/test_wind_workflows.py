3971f36815e1e92955a05c4c8d580d81a1940e96
d.franzmann <d.franzmann@fz-juelich.de>
2021-11-04 17:14:17 +0100

    update tests

--- test/05_workflows/wind_workflows/test_wind_workflows.py
+++ test/05_workflows/wind_workflows/test_wind_workflows.py
@@ -1,8 +1,9 @@
 from reskit.wind.workflows.workflows import (
     onshore_wind_merra_ryberg2019_europe,
     offshore_wind_merra_caglayan2019,
-    offshore_wind_era5,
-    onshore_wind_era5)
+    #offshore_wind_era5,
+    #onshore_wind_era5
+)
 from reskit import TEST_DATA
 import pytest
 import numpy as np
@@ -75,10 +76,12 @@ def test_offshore_wind_merra_caglayan2019(pt_wind_placements):
 @pytest.mark.skip(reason="no era5 atm")
 def test_offshore_wind_era5(pt_wind_placements):
     # placements, era5_path, output_netcdf_path=None, output_variables=None):
-    gen = offshore_wind_era5(
-        placements=pt_wind_placements,
-        era5_path=TEST_DATA['era5-like'],
-    )
+    
+    gen = None
+    #gen = offshore_wind_era5(
+    #    placements=pt_wind_placements,
+    #    era5_path=TEST_DATA['era5-like'],
+    #)
 
     assert gen.roughness.shape == (560, )
     assert np.isclose(gen.roughness.mean(), 0.0002)
@@ -101,12 +104,14 @@ def test_offshore_wind_era5(pt_wind_placements):
 @pytest.mark.skip(reason="no era5 atm")
 def test_onshore_wind_era5(pt_wind_placements):
     # placements, era5_path, gwa_100m_path, esa_cci_path, output_netcdf_path=None, output_variables=None):
-    gen = onshore_wind_era5(
-        placements=pt_wind_placements,
-        era5_path=TEST_DATA['era5-like'],
-        gwa_100m_path=TEST_DATA['gwa100-like.tif'],
-        esa_cci_path=TEST_DATA['ESA_CCI_2018_clip.tif'],
-    )
+    
+    gen=None
+    #gen = onshore_wind_era5(
+    #    placements=pt_wind_placements,
+    #    era5_path=TEST_DATA['era5-like'],
+    #    gwa_100m_path=TEST_DATA['gwa100-like.tif'],
+    #    esa_cci_path=TEST_DATA['ESA_CCI_2018_clip.tif'],
+    #)
 
     assert gen.roughness.shape == (560, )
     assert np.isclose(gen.roughness.mean(), 0.44921429)
