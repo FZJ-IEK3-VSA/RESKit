46688e012a708397370fe9bf11d10111129f2842
d.franzmann <d.franzmann@fz-juelich.de>
2021-11-30 17:44:33 +0100

    undo removing ERA5

--- reskit/wind/workflows/workflows.py
+++ reskit/wind/workflows/workflows.py
@@ -128,5 +128,138 @@ def offshore_wind_merra_caglayan2019(placements, merra_path, output_netcdf_path=
     return wf.to_xarray(output_netcdf_path=output_netcdf_path, output_variables=output_variables)
 
 
+def offshore_wind_era5(placements, era5_path, output_netcdf_path=None, output_variables=None):
+    """
+    Simulates offshore wind generation using NASA's ERA5 database [1].
+
+    Parameters
+    ----------
+    placements : pandas Dataframe
+        A Dataframe object with the parameters needed by the simulation.
+    era5_path : str
+        Path to the ERA5 data.
+    output_netcdf_path : str, optional
+        Path to a directory to put the output files, by default None
+    output_variables : str, optional
+        Restrict the output variables to these variables, by default None
+
+    Returns
+    -------
+    xarray.Dataset
+        A xarray dataset including all the output variables you defined as your output variables.
+
+    Sources
+    ------
+    [1] European Centre for Medium-Range Weather Forecasts. (2019). ERA5 dataset. https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5.
+
+    """
+    wf = WindWorkflowManager(placements)
+
+    wf.read(
+        variables=['elevated_wind_speed', ],  # Why we dont read P, T or boundary_layer_height?
+        source_type="ERA5",
+        source=era5_path,
+        set_time_index=True,
+        verbose=False)
+
+    wf.set_roughness(0.0002)
+
+    wf.logarithmic_projection_of_wind_speeds_to_hub_height()
+
+    # gaussian convolution of the power curve to account for statistical events in wind speed
+    wf.convolute_power_curves(
+                              scaling=0.01, # standard deviation of gaussian equals scaling*v + base
+                              base=0.00 # values are derived from validation with real wind turbine data
+                              )
+
+    # Adjust wind speeds
+    # elevated windspeds are corrected by a linear function by comparing to real wind turbine data.
+    # corrected_speed = windspeed * wind_speed_scaling + wind_speed_offset [m/s]
+    wind_speed_scaling=0.95
+    wind_speed_offset= 0.0
+    wf.sim_data['elevated_wind_speed'] = np.maximum(wf.sim_data['elevated_wind_speed'] * wind_speed_scaling + wind_speed_offset, 0 )
+
+    wf.simulate()
+
+    return wf.to_xarray(output_netcdf_path=output_netcdf_path, output_variables=output_variables)
+
+
+def onshore_wind_era5(placements, era5_path, gwa_100m_path, esa_cci_path, output_netcdf_path=None, output_variables=None):
+    """
+    Simulates onshore wind generation using ECMWF's ERA5 database [1]. 
+    
+    NOTE: Validation documentation is in progress...
+
+    Parameters
+    ----------
+    placements : pandas Dataframe
+        A Dataframe object with the parameters needed by the simulation.
+    era5_path : str
+        Path to the ERA5 data.
+    gwa_100m_path : str
+        Path to the Global Wind Atlas at 100m [2] rater file.
+    esa_cci_path : str
+        Path to the ESA CCI raster file [3].
+    output_netcdf_path : str, optional
+        Path to a directory to put the output files, by default None
+    output_variables : str, optional
+        Restrict the output variables to these variables, by default None
+
+    Returns
+    -------
+    xarray.Dataset
+        A xarray dataset including all the output variables you defined as your output variables.
+
+    Sources
+    ------
+    [1] European Centre for Medium-Range Weather Forecasts. (2019). ERA5 dataset. https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5
+    [2] DTU Wind Energy. (2019). Global Wind Atlas. https://globalwindatlas.info/
+    [3] ESA. Land Cover CCI Product User Guide Version 2. Tech. Rep. (2017). Available at: maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-Ph2-PUGv2_2.0.pdf
+    """
+
+    wf = WindWorkflowManager(placements)
+
+    wf.read(
+        variables=['elevated_wind_speed',
+                   "surface_pressure",
+                   "surface_air_temperature",
+                   "boundary_layer_height"],
+        source_type="ERA5",
+        source=era5_path,
+        set_time_index=True,
+        verbose=False)
+
+    wf.adjust_variable_to_long_run_average(
+        variable='elevated_wind_speed',
+        source_long_run_average=rk_weather.Era5Source.LONG_RUN_AVERAGE_WINDSPEED,
+        real_long_run_average=gwa_100m_path
+    )
+
+    wf.estimate_roughness_from_land_cover(
+        path=esa_cci_path,
+        source_type="cci")
+
+    wf.logarithmic_projection_of_wind_speeds_to_hub_height(
+            consider_boundary_layer_height=True)
+
+    wf.apply_air_density_correction_to_wind_speeds()
+
+    # gaussian convolution of the power curve to account for statistical events in wind speed
+    wf.convolute_power_curves(
+        scaling=0.01, # standard deviation of gaussian equals scaling*v + base
+        base=0.00 # values are derived from validation with real wind turbine data
+    )
+
+    # Adjust wind speeds
+    # elevated windspeds are corrected by a linear function by comparing to real wind turbine data.
+    # corrected_speed = windspeed * 0.75 + 1.2 [m/s]
+    wf.sim_data['elevated_wind_speed'] = np.maximum(wf.sim_data['elevated_wind_speed']*0.75 + 0.75, 0 ) # Empirically found to improve simulation accuracy
+
+    # do simulation
+    wf.simulate()
+
+    return wf.to_xarray(output_netcdf_path=output_netcdf_path, output_variables=output_variables)
+
+
 def mean_capacity_factor_from_sectoral_weibull(placements, a_rasters, k_rasters, f_rasters, output=None):
     pass
