b80ecd04662d572cd44f2b485f36896c5c9bd731
David Franzmann <d.franzmann@fz-juelich.de>
2021-12-03 13:21:21 +0100

    remove_ERA5

--- reskit/wind/__init__.py
+++ reskit/wind/__init__.py
@@ -18,6 +18,6 @@ from .workflows.wind_workflow_manager import WindWorkflowManager
 from .workflows.workflows import (
     onshore_wind_merra_ryberg2019_europe,
     offshore_wind_merra_caglayan2019,
-    offshore_wind_era5,
-    onshore_wind_era5
+    #offshore_wind_era5,
+    #onshore_wind_era5
 )
