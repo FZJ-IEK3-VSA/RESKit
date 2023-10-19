5bb67d152b093978769eac6c006dd709ca9c66cd
Christoph Winkler <c.winkler@fz-juelich.de>
2023-10-19 18:40:50 +0200

    #77 add automated weather tile finding and iteration as a convenience function

--- reskit/util/__init__.py
+++ reskit/util/__init__.py
@@ -5,3 +5,4 @@ from .loss_factors import low_generation_loss
 from .air_density import compute_air_density
 from .economic.lcoe import (levelized_cost_of_electricity_simplified,
                             levelized_cost_of_electricity)
+from .weather_tile import get_dataframe_with_weather_tilepaths
