7afaf7a2327541de3b26e114820b9eea640ad3e8
Christoph Winkler <c.winkler@fz-juelich.de>
2023-11-30 22:44:21 +0100

    add test for execute_workflow_iteratively

--- test/05_workflows/test_workflow_manager.py
+++ test/05_workflows/test_workflow_manager.py
@@ -1,10 +1,26 @@
 import pandas as pd
 import numpy as np
-from reskit import WorkflowQueue, WorkflowManager, distribute_workflow, TEST_DATA
+from reskit import (
+    WorkflowQueue,
+    WorkflowManager,
+    distribute_workflow,
+    execute_workflow_iteratively,
+    TEST_DATA,
+)
 import reskit as rk
 import geokit as gk
 import xarray
 import pytest
+
+
+@pytest.fixture
+def pt_wind_placements() -> pd.DataFrame:
+    df = gk.vector.extractFeatures(TEST_DATA["turbinePlacements.shp"])
+    df["hub_height"] = np.linspace(100, 130, df.shape[0])
+    df["capacity"] = 3000
+    df["rotor_diam"] = 170
+    df.loc[::2, "rotor_diam"] = 150
+    return df
 
 
 def test_WorkflowManager___init__():
@@ -437,3 +453,34 @@ def test_WorkflowQueue():
     assert results["run_2"].var2 == "cats"
     assert results["run_3"].var2 == "rectangle"
     assert results["run_4"].var2 == "plane"
+
+
+def test_execute_workflow_iteratively(pt_wind_placements):
+    gen = execute_workflow_iteratively(
+        workflow=rk.wind.onshore_wind_merra_ryberg2019_europe,
+        weather_path_varname="merra_path",
+        zoom=None,
+        # workflow_args:
+        placements=pt_wind_placements,
+        merra_path=TEST_DATA["merra-like"],
+        gwa_50m_path=TEST_DATA["gwa50-like.tif"],
+        clc2012_path=TEST_DATA["clc-aachen_clipped.tif"],
+    )
+
+    assert gen.roughness.shape == (560,)
+    assert np.isclose(gen.roughness.mean(), 0.21235714)
+    assert np.isclose(gen.roughness.min(), 0.005)
+    assert np.isclose(gen.roughness.max(), 1.2)
+    assert np.isclose(gen.roughness.std(), 0.22275803)
+
+    assert gen.elevated_wind_speed.shape == (71, 560)
+    assert np.isclose(gen.elevated_wind_speed.mean(), 7.25949888)
+    assert np.isclose(gen.elevated_wind_speed.min(), 1.65055839)
+    assert np.isclose(gen.elevated_wind_speed.max(), 14.22762999)
+    assert np.isclose(gen.elevated_wind_speed.std(), 2.48011369)
+
+    assert gen.capacity_factor.shape == (71, 560)
+    assert np.isclose(gen.capacity_factor.mean(), 0.57601636)
+    assert np.isclose(gen.capacity_factor.min(), 0.0)
+    assert np.isclose(gen.capacity_factor.max(), 0.99326205)
+    assert np.isclose(gen.capacity_factor.std(), 0.3504081)
