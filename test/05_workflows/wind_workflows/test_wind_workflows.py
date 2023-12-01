59b55c9fb4521ae86e23deef76377ced45b8c665
Christoph Winkler <c.winkler@fz-juelich.de>
2023-12-01 19:04:30 +0100

    fix test_wind_era5_2023

--- test/05_workflows/wind_workflows/test_wind_workflows.py
+++ test/05_workflows/wind_workflows/test_wind_workflows.py
@@ -156,6 +156,6 @@ def test_wind_era5_2023(pt_wind_placements):
 
     assert gen.capacity_factor.shape == (140, 560)
     assert np.isclose(gen.capacity_factor.mean(), 0.49122457)
-    assert np.isclose(gen.capacity_factor.min(), -0.001)
+    assert np.isclose(gen.capacity_factor.min(), 0)
     assert np.isclose(gen.capacity_factor.max(), 1.0)
     assert np.isclose(gen.capacity_factor.std(), 0.29246795)
