09b932ce32b70fae50f93f5ec33828a378fa0681
Christoph Winkler <c.winkler@fz-juelich.de>
2024-01-08 12:11:48 +0100

    complement parameters tests

---
+++ test/06_parameters/test_parameters.py
@@ -0,0 +1,12 @@
+from reskit.parameters.parameters import OnshoreParameters, OffshoreParameters
+from reskit import TEST_DATA
+
+def test_OnshoreParameters():
+    OnshoreParams = OnshoreParameters(fp=TEST_DATA["baseline_turbine_test.json"])
+    assert OnshoreParams.min_tip_height == 10
+    assert OnshoreParams.base_rotor_diam == 136
+
+def test_OffshoreParameters():
+    OnshoreParams = OffshoreParameters(fp=TEST_DATA["baseline_turbine_test.json"])
+    assert OnshoreParams.min_tip_height == 10
+    assert OnshoreParams.distance_to_bus == 3
