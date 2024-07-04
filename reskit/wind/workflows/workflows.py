1dce2c4455744ca6ef1d81beeaf980460621c12a
Christoph Winkler <c.winkler@fz-juelich.de>
2024-07-04 14:46:38 +0200

    #100 add generic config workflow

--- reskit/wind/workflows/workflows.py
+++ reskit/wind/workflows/workflows.py
@@ -473,3 +473,146 @@ def mean_capacity_factor_from_sectoral_weibull(
     placements, a_rasters, k_rasters, f_rasters, output=None
 ):
     pass
+
+
+def wind_config(
+    placements,
+    weather_path,
+    weather_source_type,
+    lra_ws_path, #1
+    lra_ws_scaling,
+    lra_ws_spatial_interpolation,
+    landcover_path, 
+    landcover_source_type,
+    lra_ws_nodata_fallback,
+    ws_correction_func,
+    cf_correction_factor,
+    wake_reduction_curve_name,
+    availability_factor,
+    weather_lra_path,
+    consider_boundary_layer_height,
+    power_curve_scaling,
+    power_curve_base,
+    convolute_power_curves_args={},
+    output_variables=None,
+    max_batch_size=25000,
+    output_netcdf_path=None,
+):
+    """
+    Simulates onshore and offshore (200km from shoreline) wind generation using ECMWF's ERA5 database [1].
+
+    NOTE: Validation documentation is in progress...
+
+    Parameters
+    ----------
+    placements : pandas Dataframe
+        A Dataframe object with the parameters needed by the simulation.
+    era5_path : str
+        Path to the ERA5 data.
+    gwa_100m_path : str
+        Path to the Global Wind Atlas at 100m [2] raster file.
+    esa_cci_path : str
+        Path to the ESA CCI raster file [3].
+    output_netcdf_path : str, optional
+        Path to a directory to put the output files, by default None
+    output_variables : str, optional
+        Restrict the output variables to these variables, by default None
+    nodata_fallback: str, optional
+        If no GWA available, use: (1) 'source' for ERA5 raw for simulation, (2) 'nan' for nan output
+        get flags for missing values:
+        - f'missing_values_{os.path.basename(path_to_LRA_source)}
+    correction_factor: str, float, optional
+        The wind speeds will be adapted such that the average capacity factor output is
+        scaled by the given factor. The factor may either be a float or a str formatted
+        raster path containing local float correction factors.By default 1.0, i.e. no correction.
+    max_batch_size: int
+        The maximum number of locations to be simulated simultaneously, else multiple batches will be simulated
+        iteratively. Helps limiting RAM requirements but may affect runtime. By default 25 000. Roughly 7GB RAM per 10k locations.
+    wake_reduction_curve_name : str, optional
+        string value to describe the wake reduction method. None will cause no reduction, by default
+        "dena_mean". Choose from (see more information here under wind_efficiency_curve_name[1]): "dena_mean",
+        "knorr_mean", "dena_extreme1", "dena_extreme2", "knorr_extreme1", "knorr_extreme2", "knorr_extreme3",
+    availability_factor : float, otional
+        This factor accounts for all downtimes and applies an average reduction to the output curve,
+        assuming a statistical deviation of the downtime occurences and a large enough turbine fleet.
+        By default 0.98 as suggested availability including technical availability of turbine and connector
+        as well as outages for ecological reasons (e.g. bat protection). This does not include wake effects
+        (see above) or curtailment/outage for economical reasons or transmission grid congestion.
+
+    Returns
+    -------
+    xarray.Dataset
+        A xarray dataset including all the output variables you defined as your output variables.
+
+    Sources
+    ------
+    [1] European Centre for Medium-Range Weather Forecasts. (2019). ERA5 dataset. https://www.ecmwf.int/en/forecasts/datasets/reanalysis-datasets/era5
+    [2] DTU Wind Energy. (2019). Global Wind Atlas. https://globalwindatlas.info/
+    [3] ESA. Land Cover CCI Product User Guide Version 2. Tech. Rep. (2017). Available at: maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-Ph2-PUGv2_2.0.pdf
+    """
+    assert callable(ws_correction_func), \
+        f"ws_correction_func must be an executable with a single argument that can be passed as np.array."
+
+    wf = WindWorkflowManager(placements)
+
+    # limit the input placements longitude to range of -180...180
+    assert wf.placements["lon"].between(-180, 180, inclusive="both").any()
+    # limit the input placements latitude to range of -90...90
+    assert wf.placements["lon"].between(-180, 180, inclusive="both").any()
+
+    wf.read(
+        variables=[
+            "elevated_wind_speed",
+            "surface_pressure",
+            "surface_air_temperature",
+            "boundary_layer_height",
+        ],
+        source_type=weather_source_type,
+        source=weather_path,
+        set_time_index=True,
+        verbose=False,
+    )
+
+    wf.adjust_variable_to_long_run_average(
+        variable="elevated_wind_speed",
+        source_long_run_average=weather_lra_path,
+        real_long_run_average=lra_ws_path,
+        nodata_fallback=lra_ws_nodata_fallback,
+        spatial_interpolation=lra_ws_spatial_interpolation,
+        real_lra_scaling=lra_ws_scaling,
+    )
+
+    if landcover_path:
+        wf.estimate_roughness_from_land_cover(path=landcover_path, source_type=landcover_source_type)
+
+        wf.logarithmic_projection_of_wind_speeds_to_hub_height(
+            consider_boundary_layer_height=consider_boundary_layer_height
+        )
+
+    # correct wind speeds
+    wf.sim_data["elevated_wind_speed"] = ws_correction_func(wf.sim_data["elevated_wind_speed"])
+
+
+    wf.apply_air_density_correction_to_wind_speeds()
+
+    # do wake reduction if applicable
+    wf.apply_wake_correction_of_wind_speeds(
+        wake_reduction_curve_name=wake_reduction_curve_name
+    )
+
+    # gaussian convolution of the power curve to account for statistical events in wind speed
+    wf.convolute_power_curves(
+        scaling=power_curve_scaling,  # standard deviation of gaussian equals scaling*v + base
+        base=power_curve_base,  # values are derived from validation with real wind turbine data
+        **convolute_power_curves_args,
+    )
+
+    # do simulation
+    wf.simulate(cf_correction_factor=cf_correction_factor, max_batch_size=max_batch_size)
+
+    # apply availability factor
+    wf.apply_availability_factor(availability_factor=availability_factor)
+
+    return wf.to_xarray(
+        output_netcdf_path=output_netcdf_path, output_variables=output_variables
+    )
