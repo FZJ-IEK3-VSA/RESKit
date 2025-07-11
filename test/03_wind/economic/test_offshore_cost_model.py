374634689b1b85e2eb9a64117f37d93d214df4b6
m.stargardt <m.stargardt@fz-juelich.de>
2025-07-11 11:50:02 +0200

    formatting fixed

--- test/03_wind/economic/test_offshore_cost_model.py
+++ test/03_wind/economic/test_offshore_cost_model.py
@@ -2,11 +2,9 @@ from reskit.wind.economic.offshore_cost_model import *
 import numpy as np
 
 
-
 def test_offshore_turbine_capex():
     capex = offshore_turbine_capex(
         capacity=4200, hub_height=120, rotor_diam=136, depth=30, distance_to_shore=15
     )
 
     assert np.isclose(capex / 4200, 1949.3177174432028)
-
