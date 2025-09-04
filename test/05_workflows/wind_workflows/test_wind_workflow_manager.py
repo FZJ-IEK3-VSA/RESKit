dff8ea2c683d56449c6c5c33ee9a272aacf09123
Christoph Winkler <c.winkler@fz-juelich.de>
2025-09-04 19:58:20 +0200

    add test_WindWorkflowManager_wind_shear_projection_of_wind_speeds_to_hub_height

--- test/05_workflows/wind_workflows/test_wind_workflow_manager.py
+++ test/05_workflows/wind_workflows/test_wind_workflow_manager.py
@@ -148,6 +148,26 @@ def test_WindWorkflowManager_logarithmic_projection_of_wind_speeds_to_hub_height
     assert np.isclose(man.sim_data["elevated_wind_speed"].std(), 2.9112541058945705)
 
 
+def test_WindWorkflowManager_wind_shear_projection_of_wind_speeds_to_hub_height(
+    pt_WindWorkflowManager_loaded,
+):
+    man = pt_WindWorkflowManager_loaded
+
+    man.real_lra = np.array([5.64914904, 5.42147512, 5.65448952, 5.75908499, 5.94873524]) # set hardcoded values for 100m, are usually extracted before
+
+    alternative_wind_speed_rasters={
+        50 : TEST_DATA["gwa50-like.tif"],
+        200 : TEST_DATA["gwa200-like.tif"],
+    }
+
+    man.wind_shear_projection_of_wind_speeds_to_hub_height(
+        alternative_wind_speed_rasters=alternative_wind_speed_rasters
+    )
+
+    assert np.isclose(man.sim_data["elevated_wind_speed"].mean(), 8.513871079082824)
+    assert np.isclose(man.sim_data["elevated_wind_speed"].std(), 3.0918568121980496)
+
+
 def test_WindWorkflowManager_apply_air_density_correction_to_wind_speeds(
     pt_WindWorkflowManager_loaded,
 ):
