c8839e456f797f226cc20fbb381c3c2f1f73d1bd
k-okosun <k.okosun@fz-juelich.de>
2025-10-02 15:12:11 +0200

    created independent test data and removed the hard coded paths for csp_workflows

--- test/05_workflows/csp_workflows/test_csp_workflows.py
+++ test/05_workflows/csp_workflows/test_csp_workflows.py
@@ -27,24 +27,11 @@ def pt_pv_placements() -> pd.DataFrame:
 
 # %%
 
-# @pytest.mark.skip(
-#     reason="Not working on calamari. Tested locally at 01.11.2022/d.franzmann@fz-juelich.de"
-# )
 def test_CSP_PTR_ERA5(pt_pv_placements):
-    # local
-    # era5_path = "/fast/central/shared_data/2023_gears/weather/ERA5/processed/4/7/6/2015"
-    # global_solar_atlas_dni_path = "/fast/central/shared_data/2023_gears/geography/irradiance/global_solar_atlas_v2.5/World_DNI_GISdata_LTAy_AvgDailyTotals_GlobalSolarAtlas-v2_GEOTIFF/DNI.tif"
-    # global_solar_atlas_tamb_path = "/fast/central/shared_data/2023_gears/geography/irradiance/global_solar_atlas_v2.5/World_TEMP_GISdata_LTAy_GlobalSolarAtlas_GEOTIFF/TEMP.tif"
-
-    # cluster
-    era5_path = "/fast/central/shared_data/weather_data/processed_weather_data/ERA5_global_processed_V2022.02/4/7/6/2015"
-    global_solar_atlas_dni_path = "/fast/central/shared_data/2023_gears/geography/irradiance/global_solar_atlas_v2.5/World_DNI_GISdata_LTAy_AvgDailyTotals_GlobalSolarAtlas-v2_GEOTIFF/DNI.tif"
-    global_solar_atlas_tamb_path = "/fast/central/shared_data/2023_gears/geography/irradiance/global_solar_atlas_v2.5/World_TEMP_GISdata_LTAy_GlobalSolarAtlas_GEOTIFF/TEMP.tif"
-
 
     out = rk.csp.CSP_PTR_ERA5(
         placements=pt_pv_placements,
-        era5_path=rk.TEST_DATA["csp-era5.nc"],
+        era5_path=rk.TEST_DATA["csp-era5-like"],
         global_solar_atlas_dni_path=rk.TEST_DATA["csp-gsa-dni-like.tif"],
         global_solar_atlas_tamb_path=rk.TEST_DATA["csp-gsa-temp-like.tif"],
         verbose=True,
