31fa45584117cc88dd43ddf2ed03fbf99a564abf
Christoph Winkler <c.winkler@fz-juelich.de>
2025-10-24 21:28:06 +0200

    update to latest pvlib data formats

--- reskit/solar/workflows/solar_workflow_manager.py
+++ reskit/solar/workflows/solar_workflow_manager.py
@@ -1297,7 +1297,7 @@ class SolarWorkflowManager(WorkflowManager):
         )
 
         interpolator = RectBivariateSpline(
-            _temp, _poa, gen["p_mp"].reshape(poaM.shape), kx=3, ky=3
+            _temp, _poa, np.array(gen["p_mp"]).reshape(poaM.shape), kx=3, ky=3 # np.array() since type changed between pvlib versions
         )
         self.sim_data["module_dc_power_at_mpp"] = np.zeros_like(
             self.sim_data["poa_global"]
@@ -1307,7 +1307,7 @@ class SolarWorkflowManager(WorkflowManager):
         )
 
         interpolator = RectBivariateSpline(
-            _temp, _poa, gen["v_mp"].reshape(poaM.shape), kx=3, ky=3
+            _temp, _poa, np.array(gen["v_mp"]).reshape(poaM.shape), kx=3, ky=3 # np.array() since type changed between pvlib versions
         )
         self.sim_data["module_dc_voltage_at_mpp"] = np.zeros_like(
             self.sim_data["poa_global"]
