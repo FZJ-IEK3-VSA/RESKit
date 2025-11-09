2dbac8de8e298cf391473fc19bdeaed57a184366
Christoph Winkler <c.winkler@fz-juelich.de>
2025-11-09 17:25:56 +0100

    add tests for turbine_design_from_avg_wind_speed

--- test/03_wind/core/test_design_turbine.py
+++ test/03_wind/core/test_design_turbine.py
@@ -1,10 +1,12 @@
 # from reskit import TEST_DATA
-from reskit.wind.core.design_turbine import onshore_turbine_from_avg_wind_speed
+from reskit.wind.core.design_turbine import onshore_turbine_from_avg_wind_speed, turbine_design_from_avg_wind_speed
+from reskit.wind.core.data import DATAFOLDER
+
 import numpy as np
 import pandas as pd
+import os
 
-
-def test_onshore_turbine_from_avg_wind_speed():
+def test_onshore_turbine_from_avg_wind_speed(): #TODO move most tests to test_turbine_design_from_avg_wind_speed and leave only one test for wrapper
     output = onshore_turbine_from_avg_wind_speed(wind_speed=11)
     assert isinstance(output, dict)
     assert np.isclose(output["capacity"], 5483.067776983042)
@@ -47,3 +49,77 @@ def test_onshore_turbine_from_avg_wind_speed():
     assert np.isclose(output["hub_height"], 195.98029637154556)
     assert np.isclose(output["rotor_diam"], 136)
     assert np.isclose(output["specific_power"], 270.230279939834)
+
+
+def test_turbine_design_from_avg_wind_speed():
+
+    # replicate the old onshore_turbine_from_avg_wind_speed base test
+    output = turbine_design_from_avg_wind_speed(
+        wind_speed=11,
+        technology="onshore",
+        constant_rotor_diam=None,
+        base_capacity=None,
+        base_hub_height=None,
+        base_rotor_diam=None,
+        reference_wind_speed=None,
+        min_tip_height=None,
+        min_specific_power=None,
+        max_hub_height=None,
+        tech_year=2050,
+        baseline_turbine_fp=None,
+        convention="RybergEtAl2019",    
+    )
+    assert isinstance(output, dict)
+    assert np.isclose(output["capacity"], 5483.067776983042)
+    assert np.isclose(output["hub_height"], 88.0)
+    assert np.isclose(output["rotor_diam"], 136)
+    assert np.isclose(output["specific_power"], 377.44694637315786)
+
+    # test default reference ws for spec pow, must yield default capacity and specpow
+    output = turbine_design_from_avg_wind_speed(
+        wind_speed=8.29,
+        technology="onshore",
+        constant_rotor_diam=None,
+        base_capacity=None,
+        base_hub_height=None,
+        base_rotor_diam=None,
+        reference_wind_speed=None,
+        min_tip_height=None,
+        min_specific_power=None,
+        max_hub_height=None,
+        tech_year=2035,
+        baseline_turbine_fp=os.path.join(
+            DATAFOLDER, "Baseline_plant_wind_turbine_offshore_Global_Winkler2025_v20251108.csv"
+        ),
+        convention="WinklerEtAl2026",    
+    )
+    assert isinstance(output, dict)
+    assert np.isclose(output["capacity"], 20000) # default
+    assert np.isclose(output["hub_height"], 160.34007765824106) # slightly lower than default since ref ws slightly higher than ref ws for hub height
+    assert np.isclose(output["rotor_diam"], 270)
+    assert np.isclose(output["specific_power"], 349.3112605583437) # default
+
+    output = turbine_design_from_avg_wind_speed(
+        wind_speed=7.72,
+        technology="onshore",
+        constant_rotor_diam=None,
+        base_capacity=None,
+        base_hub_height=None,
+        base_rotor_diam=None,
+        reference_wind_speed=None,
+        min_tip_height=None,
+        min_specific_power=None,
+        max_hub_height=None,
+        tech_year=2035,
+        baseline_turbine_fp=os.path.join(
+            DATAFOLDER, "Baseline_plant_wind_turbine_offshore_Global_Winkler2025_v20251108.csv"
+        ),
+        convention="WinklerEtAl2026",    
+    )
+    assert isinstance(output, dict)
+    assert np.isclose(output["capacity"], 19092.081687353104) # slightly lower than default since ref ws slightly lower than ref ws for spec pow
+    assert np.isclose(output["hub_height"], 170) # 
+    assert np.isclose(output["rotor_diam"], 270)
+    assert np.isclose(output["specific_power"], 333.4539560446091) # slightly lower than default since ref ws slightly lower than ref ws for spec pow
+
+    #TODO add tests: data year out of range, custom params as args, wrong convention, other wind speed, offshore
