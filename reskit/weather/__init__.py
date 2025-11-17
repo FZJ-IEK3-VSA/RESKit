f28e0ba1c24043cc65b57bcbbf6ba77d53292011
j.belina <j.belina@fz-juelich.de>
2025-11-17 20:00:46 +0100

    Remove import sorting

--- reskit/weather/__init__.py
+++ reskit/weather/__init__.py
@@ -1,13 +1,11 @@
-from . import util
-
-from .NCSource import NCSource
-
-from .MerraSource import MerraSource
-from .Era5Source import Era5Source
-from .SarahSource import SarahSource
-from .GSA_mean import GSAmeanSource
-from .GWA_mean import GWAmeanSource
-from .IconlamSource import IconlamSource
+from reskit.weather import util
+from reskit.weather.Era5Source import Era5Source
+from reskit.weather.GSA_mean import GSAmeanSource
+from reskit.weather.GWA_mean import GWAmeanSource
+from reskit.weather.IconlamSource import IconlamSource
+from reskit.weather.MerraSource import MerraSource
+from reskit.weather.NCSource import NCSource
+from reskit.weather.SarahSource import SarahSource
 
 # from .CosmoSource import CosmoSource  # TODO: NEEDS UPDATING!
 # from .CordexSource import CordexSourc  # TODO: NEEDS UPDATING!
