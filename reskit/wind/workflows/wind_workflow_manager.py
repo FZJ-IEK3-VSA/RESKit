120ca8be9006c8767d5c43eb245dc095ac2e68de
p.dunkel <p.dunkel@fz-juelich.de>
2025-11-18 10:51:37 +0100

    add feature related to new ws extrapolation and fix bug with loading power curves from folder path

--- reskit/wind/workflows/wind_workflow_manager.py
+++ reskit/wind/workflows/wind_workflow_manager.py
@@ -201,6 +201,25 @@ class WindWorkflowManager(WorkflowManager):
 
         elif height_scaling_method[0] == "lra":
             # we have an interpolation method based on other available LRA wind speed heights
+            
+            # if keys of height_scaling_data are str, try to convert to int or float
+            if isinstance(height_scaling_data, dict) and all(
+                [isinstance(k, str) for k in height_scaling_data.keys()]
+            ):
+                new_dict = dict()
+                for k, v in height_scaling_data.items():
+                    try:
+                        new_k = int(k)
+                    except ValueError:
+                        try:
+                            new_k = float(k)
+                        except ValueError:
+                            raise ValueError(
+                                f"height_scaling_data keys must be int or float, not {k} of type {type(k)}"
+                            )
+                    new_dict[new_k] = v
+                height_scaling_data = new_dict
+            
             lra_funcs = {
                 "linear": {
                     "func": self.wind_shear_projection_of_wind_speeds_to_hub_height,
