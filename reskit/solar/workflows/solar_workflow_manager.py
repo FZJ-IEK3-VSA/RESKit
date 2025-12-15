86c0a5b4c04d668e3660278fbe0f5373932e0f2f
Christoph Winkler <c.winkler@fz-juelich.de>
2025-12-15 17:37:54 +0100

    remove git merge duplicate

--- reskit/solar/workflows/solar_workflow_manager.py
+++ reskit/solar/workflows/solar_workflow_manager.py
@@ -1062,9 +1062,6 @@ class SolarWorkflowManager(WorkflowManager):
         index_out = (dni_flat < 25) & (np.cos(zen) < 0.05)
         self.sim_data["direct_normal_irradiance"][index_out] = 0
 
-        index_out = (dni_flat < 25) & (np.cos(zen) < 0.05)
-        self.sim_data["direct_normal_irradiance"][index_out] = 0
-
         sel = ~np.isfinite(self.sim_data["direct_normal_irradiance"])
         sel = np.logical_or(sel, self.sim_data["direct_normal_irradiance"] < 0)
         sel = np.logical_or(sel, self.sim_data["direct_normal_irradiance"] > 1600)
