07cb344dbd2f691bee930b46e2c97dc009e0d782
m.stargardt <m.stargardt@fz-juelich.de>
2026-03-20 12:06:55 +0100

    base function checked

--- test/03_wind/economic/test_offshore_cost_model.py
+++ test/03_wind/economic/test_offshore_cost_model.py
@@ -66,16 +66,52 @@ def test_calculateSpecificOffshoreCapex():
 
 
 def test_getSpecificOffshoreCableCost():
-    value1 = getSpecificOffshoreCableCost(10, 14000, variableCostFactor=1.35, fixedCost=0)
+    c1 = getSpecificOffshoreCableCost(
+        distance=1000,
+        capacity= 14000,
+        voltageType='dc',
+        variableCostFactor=1.35,
+        fixedCost=0,
+        year=2050)
+    assert np.isclose(c1, 1350), "Error in getCableCostfuncion, possibly due to adapted function"
 
-    value2 = getSpecificOffshoreCableCost(10, 10000, variableCostFactor=1.35, fixedCost=0)
+    c2 = getSpecificOffshoreCableCost(
+        distance=np.array([10000, 2000, 3000]),
+        capacity= np.array([10000, 5, 5000]),
+        voltageType=np.array(['dc', 'ac', 'dc']),
+        variableCostFactor=np.array([2, 0.4, 20]),
+        fixedCost=0,
+        year=2050)
+    expected = np.array([20000, 800, 60000])
 
-    assert np.isclose(value1, 189000), "Error in getCableCostfuncion, possibly due to adapted function"
-    assert np.isclose(value2, 135000), "Error in getCableCostfuncion, possibly due to adapted function"
+    np.testing.assert_allclose(c2, expected, rtol=0.05), "Error in getCableCostfuncion, possibly due to adapted function"
+
+
 
 
 def test_getOffshoreTurbineFoundationCost():
-    pass
+    
+    c1=getOffshoreTurbineFoundationCost(
+        depth=10.8,
+        maxMonopileDepth=47.9,
+        maxJacketDepth=60,
+        year=2050,
+        returnType=False
+    )  
+    assert np.isclose(c1, 195.57, rtol=0.05), "Error in getoffshoreTurbineFoundationCostFunction, possibly due to adapted function"
+
+    c2=getOffshoreTurbineFoundationCost(
+        depth=np.array([10, 30, 60]),
+        maxMonopileDepth=25,
+        maxJacketDepth=55,
+        year=2050,
+        returnType=False
+    )
+
+    expected=np.array([192.31, 301.4, 883.48])
+    np.testing.assert_allclose(c2, expected, rtol=0.05), "Error in getoffshoreTurbineFoundationCostFunction, possibly due to adapted function"
+
+
 
 
 def test_getSpecificOffshorePlatformCost():
@@ -151,29 +187,43 @@ def test_getSpecificOffshorePlatformCost():
 
 
 def test_getSpecificConverterStationCost():
-    # test onshore AC substation
+ 
     c1 = getSpecificConverterStationCost(
         capacity=10000,
         waterDepth=20,  
-        portDistance=40,
-        foundationType= "jacket",
+        voltageType="ac",
+        portDistance=1000,
         maxJacketDepth= 55,
         convention="RogeauEtAl2023",
     )
-    # i wanmt to check wheter it is close to c1 andf not exactly c1, because the function is adapted and not exactly the same as in the paper, but the cost should be in the same order of magnitude
-    assert np.isclose(c1, 1130, rtol=0.1)
+
+    assert np.isclose(c1, 246, rtol=0.05)
 
 
-    # test offshore DC substation
+    # test None as waterdepth
     c2 = getSpecificConverterStationCost(
         capacity=10000,
-        waterDepth=55,  # jacket depth
-        voltageType="dc",
-        maxJacketDepth=55,
+        waterDepth=None,  
+        voltageType="ac",
+        portDistance=1000,
+        maxJacketDepth= 55,
         convention="RogeauEtAl2023",
     )
 
-    assert np.isclose(c2, 177, rtol=0.1)
+    assert np.isclose(c2, 23.2, rtol=0.05)
+    # array test
+    c3 = getSpecificConverterStationCost(
+        capacity=np.array([10000,20000,30000]),
+        waterDepth=np.array([20, 25, 30]),
+        voltageType=np.array(["ac", "ac", "dc"]),
+        portDistance=np.array([1000, 3000, 10]),
+        maxJacketDepth= 55,
+        convention="RogeauEtAl2023",
+    )
+
+    expected = np.array([245.92559009, 238.06263514, 193.16463363])
+
+    np.testing.assert_allclose(c3, expected, rtol=0.05)
 
     # TEST MUST-FAIL CASES
 
@@ -181,11 +231,24 @@ def test_getSpecificConverterStationCost():
         getSpecificConverterStationCost(
             capacity=10000,
             waterDepth=55,  # jacket depth
-            voltageType="does_not_exist",  # must fail
+            voltageType="does_not_exist", 
+            portDistance=1000, # must fail
             maxJacketDepth=55,
             convention="RogeauEtAl2023",
         )
 
 
 def test_getSpecificOffshoreConnectionCost():
-    pass
+    c1= getSpecificOffshoreConnectionCost(
+        capacity=np.array([10000,20000,30000]),
+        waterDepth=np.array([20, 25, 30]),
+        coastDistance=np.array([1000, 3000, 10]),
+        year=2050,
+        voltageType='ac',
+        portDistance=np.array([1000, 3000, 10]),
+        baseWFSize=20000,
+        maxJacketDepth= 55,
+    )
+    expected = (np.array([7135.17077252, 21411.09138514,   261.09967568]),'ac')
+    np.testing.assert_allclose(c1[0], expected[0], rtol=0.05), "Error in getSpecificOffshoreConnectionCost, possibly due to adapted function"
+    assert c1[1] == expected[1], "Error in getSpecificOffshoreConnectionCost, possibly due to adapted function"
