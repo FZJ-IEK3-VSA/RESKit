03e0215d71aba14238132ad63a5f1457af7775f8
Christoph Winkler <64524976+chrisjwin@users.noreply.github.com>
2026-07-09 16:07:04 +0200

    Refactor wind_config to conditionally adjust LRA

--- reskit/wind/workflows/workflows.py
+++ reskit/wind/workflows/workflows.py
@@ -399,8 +399,8 @@ def wind_config(
     placements,
     weather_path,
     weather_source_type,
-    enable_lra_adjustment,
     weather_lra_ws_path,
+    enable_lra_adjustment,
     real_lra_ws_path,
     real_lra_ws_scaling,
     real_lra_ws_spatial_interpolation,
@@ -433,14 +433,14 @@ def wind_config(
         A Dataframe object with the parameters needed by the simulation.
     weather_path : str
         Path to the temporally resolved weather data, e.g. ERA-5 or MERRA-2 etc.
-    enable_lra_adjustment : bool
-        If True, enables the long-run-average adjustment
-        using the provided lra information.
-        Note: If False, all lra related parameters have no effect.
     weather_lra_ws_path : str
         The path to a raster with the corresponding long-run-average
         windspeeds of the actual weather data (will be corrected to the
         real lra if given, else weather_lra_path has no effect)
+    enable_lra_adjustment : bool
+        If True, enables the long-run-average adjustment
+        using the provided lra information.
+        Note: If False, all lra related parameters have no effect.
     real_lra_ws_path : str, float
         Either a float/int (1.0 means no scaling) or a path to a raster
         with real long-run-average wind speeds, e.g. the Global Wind Atlas
@@ -566,6 +566,7 @@ def wind_config(
     assert callable(ws_correction_func), (
         f"ws_correction_func must be an executable with a single argument that can be passed as np.array (if not 1)."
     )
+    assert isinstance(enable_lra_adjustment, bool), "enable_lra_adjustment must be boolean."
 
     wf = WindWorkflowManager(placements)
 
@@ -582,15 +583,15 @@ def wind_config(
         verbose=False,
     )
 
-    wf.adjust_variable_to_long_run_average(
-        variable="elevated_wind_speed",
-        source_long_run_average=weather_lra_ws_path,
-        real_long_run_average=real_lra_ws_path,
-        nodata_fallback=real_lra_ws_nodata_fallback,
-        spatial_interpolation=real_lra_ws_spatial_interpolation,
-        real_lra_scaling=real_lra_ws_scaling,
-        enable_lra_adjustment=enable_lra_adjustment,
-    )
+    if enable_lra_adjustment:
+        wf.adjust_variable_to_long_run_average(
+            variable="elevated_wind_speed",
+            source_long_run_average=weather_lra_ws_path,
+            real_long_run_average=real_lra_ws_path,
+            nodata_fallback=real_lra_ws_nodata_fallback,
+            spatial_interpolation=real_lra_ws_spatial_interpolation,
+            real_lra_scaling=real_lra_ws_scaling,
+        )
 
     if height_scaling_method is not None:
         if isinstance(height_scaling_method, list):
