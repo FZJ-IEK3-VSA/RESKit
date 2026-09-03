527182368b8d58c571a252022b0d402e6cb210c0
p.dunkel <p.dunkel@fz-juelich.de>
2026-09-03 14:08:58 +0200

    remove unnecessary comment

--- reskit/weather/Era5Source/Era5Source.py
+++ reskit/weather/Era5Source/Era5Source.py
@@ -168,7 +168,6 @@ class Era5Source(NCSource):
         "forecast_surface_roughness": "fsr",
         "surface_solar_radiation_downwards": "ssrd",
         "total_sky_direct_solar_radiation_at_surface": "fdir",
-        # no workflow requires the snow variables, but Era5Source can load them
         "snow_albedo": "asn",
         "snow_density": "rsn",
         "snow_depth": "sd",
